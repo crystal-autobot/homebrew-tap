47c6faa89f0353c2cfc95d4b609fcf6a283b213e
Vitalii Elenhaupt <velenhaupt@gmail.com>
2026-02-13 18:43:55 +0100

    Initial commit

--- autobot.rb
+++ autobot.rb
@@ -1,18 +1,17 @@
 class Autobot < Formula
   desc "Ultra-efficient personal AI assistant powered by Crystal"
   homepage "https://github.com/crystal-autobot/autobot"
-  version "0.1.0"
+  version "VERSION_PLACEHOLDER"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/crystal-autobot/autobot/releases/download/v0.1.0/autobot-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      url "https://github.com/crystal-autobot/autobot/releases/download/vVERSION_PLACEHOLDER/autobot-darwin-arm64.tar.gz"
+      sha256 "SHA256_PLACEHOLDER_ARM64"
     end
 
     on_intel do
-      url "https://github.com/crystal-autobot/autobot/releases/download/v0.1.0/autobot-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_AMD64_SHA256"
+      odie "Autobot only supports Apple Silicon (ARM64) Macs. Intel Macs are not supported."
     end
   end
 
